f1b63d1ffde77f8f91bce6d32df2ea555fe3d6b1
ThamNguyenDinh <nguyendinhtham94qn@gmail.com>
2026-07-07 13:52:40 +0700

    Add salary

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -191,16 +191,16 @@
             {{ __('ui.nav.reports_ot') }}
         </a>
         @endif
-{{--        @if($canSee('reports.leave'))--}}
-{{--        <a href="{{ route('admin.reports.leave') }}"--}}
-{{--           class="flex items-center px-3 py-2.5 rounded-lg font-medium transition-colors--}}
-{{--                  {{ request()->routeIs('admin.reports.leave*') ? 'bg-gray-100 text-gray-900 font-bold' : 'text-gray-600 hover:bg-gray-50' }}">--}}
-{{--            <svg class="w-5 h-5 mr-3 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">--}}
-{{--                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"/>--}}
-{{--            </svg>--}}
-{{--            {{ __('ui.nav.reports_leave') }}--}}
-{{--        </a>--}}
-{{--        @endif--}}
+        @if($canSee('reports.leave'))
+        <a href="{{ route('admin.reports.leave') }}"
+           class="flex items-center px-3 py-2.5 rounded-lg font-medium transition-colors
+                  {{ request()->routeIs('admin.reports.leave*') ? 'bg-gray-100 text-gray-900 font-bold' : 'text-gray-600 hover:bg-gray-50' }}">
+            <svg class="w-5 h-5 mr-3 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"/>
+            </svg>
+            {{ __('ui.nav.reports_leave') }}
+        </a>
+        @endif
         {{-- Admin Roles: manager only --}}
         @if($allowedModules === null)
         <a href="{{ route('admin.admin-roles.index') }}"
